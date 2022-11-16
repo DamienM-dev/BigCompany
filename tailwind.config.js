736a27f3cd074e552609b8c682426c8104826c44
Damien MIREMONT <damien.miremont@gmail.com>
2022-11-16 13:11:59 +0100

    ajout d'une partie du dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
       colors: {
         'primary': '#f1f5f9',
         'secondary': '#ffc100',
-        'tertiary': '#17c7be'
+        'tertiary': '#17c7be',
+        'primaryDark': '##1c1b1b',
+        'secondaryDark':'#e85d04',
+        'tertiaryDark':'#000814',
       },
       fontFamily: {
         'OpenSans': ['Open Sans'],
         'Roboto':['Roboto'],
       },
+      darkMode: 'media',
     },
     container: {
       center: true,
