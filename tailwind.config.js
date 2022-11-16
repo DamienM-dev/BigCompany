ac676646cd82c93979246aa8d41a25e8f95d4de2
Damien MIREMONT <damien.miremont@gmail.com>
2022-11-16 18:35:42 +0100

    terminé à 98% + ajout d'un read me

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
   content: ['./*/*.html'],
   theme: {
     extend: {
+      
       colors: {
         'primary': '#f1f5f9',
         'secondary': '#ffc100',
         'tertiary': '#17c7be',
-        'primaryDark': '##1c1b1b',
+        'primaryDark': '#212529',
         'secondaryDark':'#e85d04',
         'tertiaryDark':'#000814',
       },
@@ -15,11 +16,16 @@ module.exports = {
         'OpenSans': ['Open Sans'],
         'Roboto':['Roboto'],
       },
-      darkMode: 'media',
+      borderRadius: {
+        '4xl': '3.5rem',
+      }
     },
+    darkMode: 'media',
+
     container: {
       center: true,
     },
+    
   },
   plugins: [],
 }
